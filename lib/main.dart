662681fa4cd1d7866f50e17539ba34e093ea4680
Wafeey HM <wafiy@anak.it>
2022-06-23 14:08:01 +0800

    column-row-widget
    testing out main axis alignment

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
               Container(
                 height: 100.0,
